40fd92a004588a9d0b3e2ed2d8964311de7aa461
HyTest32232 <1826899126@qq.com>
2022-07-25 16:06:24 +0800

    新增添加成员测试用例

--- Web/page_object/my_page.py
+++ Web/page_object/my_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from Web.page_object.base_page import BasePage
+from Web.page_object.organization_manage_page import OrganizationPage
 from Web.page_object.video_manage_page import VideoPage
 
 
@@ -7,3 +8,7 @@ class MyPage(BasePage):
     def go_to_video_manage(self):
         self.find(By.CSS_SELECTOR,'#app > div > div:nth-child(2) > div > div.left > div > div.barlist > div:nth-child(4)').click()
         return VideoPage(self.driver)
+
+    def go_to_organzation_manage(self):
+        self.find(By.CSS_SELECTOR,'#app > div > div:nth-child(2) > div > div.left > div > div.barlist > div:nth-child(1)').click()
+        return OrganizationPage(self.driver)
--- Web/testcase/test_create.py
+++ Web/testcase/test_create.py
@@ -6,3 +6,8 @@ class TestCreateVideo:
         index = IndexPage()
         phone_list = index.go_to_my_page().go_to_video_manage().create_get_video("test_selenium")
         assert "test_selenium" in phone_list
+
+    def test_add_member(self):
+        index = IndexPage()
+        users = index.go_to_my_page().go_to_organzation_manage().add_members()
+        assert ('test1','hybb'),('test_vote全体','hybb') in users
